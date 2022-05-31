63659db1beb8740da093d4427a96110908b1248b
AlphaGo <98372774+AlphaGoJava@users.noreply.github.com>
2022-05-31 17:41:42 +0900

    하 .. 캘린더 최종 끝
    하 .. 캘린더 최종 끝

--- app/src/main/java/com/example/anafor/Nav_Schedule/ScheduleActivity.java
+++ app/src/main/java/com/example/anafor/Nav_Schedule/ScheduleActivity.java
@@ -10,6 +10,7 @@ import android.text.style.ForegroundColorSpan;
 
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -50,12 +51,14 @@ public class ScheduleActivity extends AppCompatActivity {
     TextView tv_schedule_insert, tv_schedule_diary_date;
     LinearLayout container_schedule;
     ArrayList<CalendarDay> dates = new ArrayList<>();
+    LinearLayout ln_cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
-
+        
+        ln_cal = findViewById(R.id.ln_cal);
         tv_schedule_insert = findViewById(R.id.tv_schedule_insert);
         tv_schedule_diary_date = findViewById(R.id.tv_schedule_diary_date);
         imgv_my_allim_back = findViewById(R.id.imgv_my_allim_back);
@@ -77,57 +80,9 @@ public class ScheduleActivity extends AppCompatActivity {
                         new ScheduleFragment1(tv_schedule_diary_date.getText() + "")).commit();
             } // onClick
         }); // setOnClickListener
-
-        calendarView = findViewById(R.id.calendarview);
-        
-        // 최소, 최대의 월 를
-        // minusMonths, plusMonths 로 표현해줌
-        calendarView.state()
-                .edit()
-                .setFirstDayOfWeek(DayOfWeek.of(Calendar.SATURDAY))
-                .setMinimumDate(LocalDate.now().minusMonths(3))
-                .setMaximumDate(LocalDate.now().plusMonths(24))
-                .commit();
-
-        // 선택된 날짜의 배경 색상
-        calendarView.setSelectionColor(Color.parseColor("#696aad"));
-
-        // 월, 일을 한글로 보이게 설정 => Strings 참고
-        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
-        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
-
         select();   // 조회를 반복적으로 사용하기 위해 메소드를 만들어서 사용했음
-
-        // addDecorators 를 이용해서 일요일, 토요일에 색상을 주고
-        // 특정일에 선택이 되게끔 데이터 넘김
-        calendarView.addDecorators(new SundayDecorator(), new SaturdayDecorator(), new EventDecorator(Color.RED, dates,this));
-
-        // 처음에는 오늘 날짜를 선택되게 해주었음
-        calendarView.setSelectedDate(CalendarDay.today());
-
-        // 첫 화면 들어갔을 때 오늘 날짜로 데이터가 있으면
-        // 그에 해댕하는 데이터를 보여주게 끔 처리
-        tv_schedule_diary_date.setText(String.format(LocalDate.now().getYear() + "년 " +LocalDate.now().getMonthValue() + "월 " + LocalDate.now().getDayOfMonth() + "일 " ));
-        getSupportFragmentManager().beginTransaction().replace(R.id.container_schedule, new ScheduleFragment2(tv_schedule_diary_date.getText() + "")).commit();
-
-        // 원하는 날짜를 클릭시 setText 로 date 를 찍어주는 이벤트와
-        // 그에 맞는 데이터를 조회하는 부분인 fragment2 로 넘겨주고
-        // 해당하는 데이터를 뿌려주게끔 하였음
-        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
-            @SuppressLint("ResourceType")
-            @Override
-            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-                tv_schedule_diary_date.setText(String.format(date.getYear() + "년 " + date.getMonth() + "월 " + date.getDay() + "일 " ));
-                // 선택한 일정 마다 onDateSelected 를 할 때 그 해당되는 날짜의 데이터가 있는지 여부를
-                // 판단하기 위해 fragment2 에서 다시 조회를 하게 한 다음,
-                // 다시 조회한 데이터를 보여주게 하기 위해 getSupport 로 다시 전환 해주었음
-                getSupportFragmentManager().beginTransaction().replace(R.id.container_schedule, new ScheduleFragment2(tv_schedule_diary_date.getText() + "")).commit();
-            }// onDateSelected
-        });// setOnDateChangedListener
-        
     }// onCreate
-
-
+    
     // 일요일 모양주기
     public class SundayDecorator implements DayViewDecorator {
 
@@ -144,8 +99,8 @@ public class ScheduleActivity extends AppCompatActivity {
             view.addSpan(new ForegroundColorSpan(Color.RED));
 
         }// decorate
-    }// SundayDecorator
 
+    }// SundayDecorator
 
     // 토요일 모양주기
     public class SaturdayDecorator implements DayViewDecorator {
@@ -191,26 +146,6 @@ public class ScheduleActivity extends AppCompatActivity {
         }// decorate
     }// EventDecorator
 
-
-    public class RemoveDecorator implements DayViewDecorator {
-        private final HashSet<CalendarDay> dates;
-
-        public RemoveDecorator(Collection<CalendarDay> dates) {
-            this.dates = new HashSet<>(dates);
-        }
-
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return dates.contains(day);
-            // also tried with just
-            // return false;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            // TODO: what to do?
-        }
-    }
     public void changeFragment(Fragment frag){
         select();
         getSupportFragmentManager().beginTransaction().replace(R.id.container_schedule, frag).commit();
@@ -220,6 +155,8 @@ public class ScheduleActivity extends AppCompatActivity {
     // 특정일에 파란색 점을 찍기 위하여
     // 필요한 데이터를 DB 에서 조회를 해옴
     public void select() {
+        dates.clear(); // 리스트를 조회 하기 전에 한번 비워주고 다시 조회 하게 끔
+        setCal();
         Gson gson = new Gson();
         AskTask task = new AskTask("/schedule_select");
         task.addParam("select", "admin");
@@ -239,15 +176,72 @@ public class ScheduleActivity extends AppCompatActivity {
                 month = Integer.parseInt(dateData.substring(dateData.indexOf("년") + 1, dateData.indexOf("월")).trim());
                 day = Integer.parseInt(dateData.substring(dateData.indexOf("월") + 1, dateData.indexOf("일")).trim());
                 dates.add(CalendarDay.from(year, month, day));
-              //  calendarView.removeDecorators();
                 calendarView.addDecorators(new SundayDecorator(), new SaturdayDecorator(), new EventDecorator(Color.RED, dates,this));
             } // for
         } // if
+        setCal();
     }
     @Override
     protected void onResume() {
         super.onResume();
         select();
     }
+
+    public void setCal(){
+        ln_cal.removeAllViews();
+        // xml 에서 LinearLayout 으로 칸을 지정 해 놓은 다음,
+        // 엑티비티에서 xml 작업을 진행하였음
+        // 삭제 할 때 밑에 점 표시도 같이 지워져야 하는데
+        // 일종의 강제성으로 여기에 xml 작업을 한 후 메소드를 호출해서 필요할 때 마다 
+        // 다시 캘린더 메소드를 조회 할 수 있게 하였음
+        LinearLayout.LayoutParams parambtn = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        calendarView = new MaterialCalendarView(ScheduleActivity.this);
+        calendarView.setLayoutParams(parambtn);
+        ln_cal.addView(calendarView);
+
+        // 최소, 최대의 월 를
+        // minusMonths, plusMonths 로 표현해줌
+        calendarView.state()
+                .edit()
+                .setFirstDayOfWeek(DayOfWeek.of(Calendar.SATURDAY))
+                .setMinimumDate(LocalDate.now().minusMonths(3))
+                .setMaximumDate(LocalDate.now().plusMonths(24))
+                .commit();
+
+        // 선택된 날짜의 배경 색상
+        calendarView.setSelectionColor(Color.parseColor("#696aad"));
+
+        // 월, 일을 한글로 보이게 설정 => Strings 참고
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+
+        // addDecorators 를 이용해서 일요일, 토요일에 색상을 주고
+        // 특정일에 선택이 되게끔 데이터 넘김
+        calendarView.addDecorators(new SundayDecorator(), new SaturdayDecorator(), new EventDecorator(Color.RED, dates,this));
+
+        // 처음에는 오늘 날짜를 선택되게 해주었음
+        calendarView.setSelectedDate(CalendarDay.today());
+
+        // 첫 화면 들어갔을 때 오늘 날짜로 데이터가 있으면
+        // 그에 해댕하는 데이터를 보여주게 끔 처리
+        tv_schedule_diary_date.setText(String.format(LocalDate.now().getYear() + "년 " +LocalDate.now().getMonthValue() + "월 " + LocalDate.now().getDayOfMonth() + "일 " ));
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_schedule, new ScheduleFragment2(tv_schedule_diary_date.getText() + "")).commit();
+
+        // 원하는 날짜를 클릭시 setText 로 date 를 찍어주는 이벤트와
+        // 그에 맞는 데이터를 조회하는 부분인 fragment2 로 넘겨주고
+        // 해당하는 데이터를 뿌려주게끔 하였음
+        calendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                tv_schedule_diary_date.setText(String.format(date.getYear() + "년 " + date.getMonth() + "월 " + date.getDay() + "일 " ));
+                // 선택한 일정 마다 onDateSelected 를 할 때 그 해당되는 날짜의 데이터가 있는지 여부를
+                // 판단하기 위해 fragment2 에서 다시 조회를 하게 한 다음,
+                // 다시 조회한 데이터를 보여주게 하기 위해 getSupport 로 다시 전환 해주었음
+                getSupportFragmentManager().beginTransaction().replace(R.id.container_schedule, new ScheduleFragment2(tv_schedule_diary_date.getText() + "")).commit();
+            }// onDateSelected
+        });// setOnDateChangedListener
+    }// setCal
 
 }// class ScheduleActivity
