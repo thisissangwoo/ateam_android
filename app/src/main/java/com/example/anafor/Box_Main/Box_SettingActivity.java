327af329ca10f6bbb51885a9f7d5b4987e51aa33
eun-yeong <zerotey54@gmail.com>
2022-06-09 15:56:57 +0900

    iot 웹뷰 보이기

--- app/src/main/java/com/example/anafor/Box_Main/Box_SettingActivity.java
+++ app/src/main/java/com/example/anafor/Box_Main/Box_SettingActivity.java
@@ -2,20 +2,31 @@ package com.example.anafor.Box_Main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.anafor.R;
 
 public class Box_SettingActivity extends AppCompatActivity {
 
     ImageView imgv_box_setting_back;
+    TextView tv_iotmap;                     //웹뷰로 이동할 텍스트뷰
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_box_setting);
+        tv_iotmap = findViewById(R.id.tv_iotmap);
+        tv_iotmap.setOnClickListener(new View.OnClickListener() {   //클릭시 웹뷰로 이동
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Box_SettingActivity.this, WebMapActivity.class);
+                startActivity(intent);
+            }
+        });
 
         imgv_box_setting_back = findViewById(R.id.imgv_box_setting_back);
 
